0471d30b4da47e370e6b2a27a840800eacc57611
Baturay <baturayturan@gmail.com>
2024-06-27 21:49:50 +0200

    made text argument optional

--- src/batFramework/gui/button.py
+++ src/batFramework/gui/button.py
@@ -7,7 +7,7 @@ from math import ceil
 
 
 class Button(Label, ClickableWidget):
-    def __init__(self, text: str, callback: None | Callable = None) -> None:
+    def __init__(self, text: str="", callback: None | Callable = None) -> None:
         super().__init__(text=text)
         self.set_callback(callback)
 
