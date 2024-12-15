5e16afbf10bb6fb249c861ea73728687a0bfe856
bturan <baturay.turan@edu.univ-fcomte.fr>
2024-12-15 04:39:37 +0100

    added new stuff

--- src/batFramework/cutscene.py
+++ src/batFramework/cutscene.py
@@ -51,3 +51,69 @@ class TransitionToScene(Cutscene):
     def start(self):
         bf.CutsceneManager().manager.transition_to_scene(self.scene_name,self.transition)
         bf.Timer(self.transition.duration,end_callback=self.end).start()
+
+
+
+class GlideWorldCameraTo(Cutscene):
+    def __init__(self, stop:tuple[float,float],duration:float=1,easing:bf.easing=bf.easing.EASE_IN_OUT,scene_name:str=None):
+        super().__init__()
+        self.scene =  None
+        self.scene_name = scene_name
+        self.stop_pos = stop
+        self.start_pos = None
+        self.controller = bf.EasingController(easing,duration,update_callback=self.internal,end_callback=self.end)
+
+
+    def start(self):
+        if self.scene_name is None:
+            self.scene_name = bf.CutsceneManager().manager.get_scene_at(0).name
+
+        self.scene = bf.CutsceneManager().manager.get_scene(self.scene_name)
+        self.start_pos = self.scene.camera.get_center()
+        self.controller.start()
+
+
+    def internal(self,progression:float):
+        self.scene.camera.set_center(
+            self.start_pos[0]+progression*(self.stop_pos[0]-self.start_pos[0]),
+            self.start_pos[1]+progression*(self.stop_pos[1]-self.start_pos[1])
+
+        )
+
+    def end(self):
+        if self.scene:
+            self.scene.camera.set_center(self.stop_pos[0],self.stop_pos[1])
+        
+        super().end()
+
+
+class GlideWorldCameraFromTo(Cutscene):
+    def __init__(self,start:tuple[float,float], stop:tuple[float,float],duration:float=1,easing:bf.easing=bf.easing.EASE_IN_OUT,scene_name:str=None):
+        super().__init__()
+        self.scene =  None
+        self.scene_name = scene_name
+        self.start_pos = start
+        self.stop_pos = stop
+        self.controller = bf.EasingController(easing,duration,update_callback=self.internal,end_callback=self.end)
+
+
+    def start(self):
+        if self.scene_name is None:
+            self.scene_name = bf.CutsceneManager().manager.get_scene_at(0).name
+
+        self.scene = bf.CutsceneManager().manager.get_scene(self.scene_name)
+        self.controller.start()
+
+
+    def internal(self,progression:float):
+        self.scene.camera.set_center(
+            self.start_pos[0]+progression*(self.stop_pos[0]-self.start_pos[0]),
+            self.start_pos[1]+progression*(self.stop_pos[1]-self.start_pos[1])
+
+        )
+
+    def end(self):
+        if self.scene:
+            self.scene.camera.set_center(self.stop_pos[0],self.stop_pos[1])
+        
+        super().end()
